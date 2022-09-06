ddad816fd666015c75b05b466d7330f883a0f0e7
aigouzz <1003823149@qq.com>
2022-09-06 12:19:27 +0800

    添加compiler

---
+++ src/v-src/compiler/index.js
@@ -0,0 +1,24 @@
+/**
+ * 编译器
+ * template中原生html和非原生的元素编译，也就是render函数，render将模板生成对应vnode，vnode经过patch得到将要渲染的VNode，
+ * 根据渲染VNode生成真实的dom节点插入到视图中，完成更新
+ * 用户写的模板 -- 》模板编译--》render函数--》VNode--》patch--》视图
+ * 
+ * AST:abstract syntax tree
+ * 抽象语法树：
+ * 流程：1：模板解析阶段：将一堆字符串用正则等方式解析成抽象语法树ast  src/compiler/parser/index.js
+ * 2：优化阶段：遍历ast，找出其中静态节点，并做标记 src/compiler/optimiser.js
+ * 3：代码生成阶段：将ast转换成渲染函数  src/compiler/codegen/index.js
+ * 
+ */
+
+export const createCompiler = createCompilerCreator(function(
+    template,
+    options
+) {
+    let ast = parse(template.trim(), options);
+    if(options.optimize !== false) {
+        optimize(ast, options);
+    }
+    let code = generate(ast, options);
+});
--- src/v-src/core/observer/VNode.js
+++ src/v-src/core/observer/VNode.js
@@ -4,8 +4,6 @@
  * 新的vnode中没有，旧的vnode中有，就在旧的vnode中删除
  * 都有，就以新的vnode为主，更新
  */
-i
-
 import { isDef, isUndef, SSR_ATTR, isRegExp, traverse } from "./api";
 
 function makeMap (
@@ -741,22 +739,68 @@ export function createPatchFunction(backend) {
                         oldVnode.removeAttribute(SSR_ATTR);
                         hydrating = true;
                     }
+                    if(hydrating) {
+                        if(hydrate(oldVnode, vnode, insertedVnodeQueue)) {
+                            invokeInsertHook(vnode, insertedVnodeQueue, true);
+                            return oldVnode;
+                        } else {
+                            console.warn(
+                                'The client-side rendered virtual DOM tree is not matching ' +
+                            'server-rendered content. This is likely caused by incorrect ' +
+                            'HTML markup, for example nesting block-level elements inside ' +
+                            '<p>, or missing <tbody>. Bailing hydration and performing ' +
+                            'full client-side render.'
+                            );
+                        }
+                    }
+                    oldVnode = emptyNodeAt(oldVnode);
                 }
-                if(hydrating) {
-                    if(hydrate(oldVnode, vnode, insertedVnodeQueue)) {
-                        invokeInsertHook(vnode, insertedVnodeQueue, true);
-                        return oldVnode;
-                    } else {
-                        console.warn(
-                            'The client-side rendered virtual DOM tree is not matching ' +
-                        'server-rendered content. This is likely caused by incorrect ' +
-                        'HTML markup, for example nesting block-level elements inside ' +
-                        '<p>, or missing <tbody>. Bailing hydration and performing ' +
-                        'full client-side render.'
-                        );
+                let oldElm = oldVnode.elm;
+                let parentElm = nodeOps.parentNode(oldElm);
+                createElm(
+                    vnode,
+                    insertedVnodeQueue,
+                    // extremely rare edge case: do not insert if old element is in a
+                    // leaving transition. Only happens when combining transition +
+                    // keep-alive + HOCs. (#4590)
+                    oldElm._leaveCb ? null : parentElm,
+                    nodeOps.nextSibling(oldElm)
+                );
+                if (isDef(vnode.parent)) {
+                    let ancestor = vnode.parent;
+                    let patchable = isPatchable(vnode);
+                    while (ancestor) {
+                        for (let i = 0; i < cbs.destroy.length; ++i) {
+                            cbs.destroy[i](ancestor);
+                        }
+                        ancestor.elm = vnode.elm;
+                        if (patchable) {
+                            for (let i$1 = 0; i$1 < cbs.create.length; ++i$1) {
+                                cbs.create[i$1](emptyNode, ancestor);
+                            }
+                            // #6513
+                            // invoke insert hooks that may have been merged by create hooks.
+                            // e.g. for directives that uses the "inserted" hook.
+                            var insert = ancestor.data.hook.insert;
+                            if (insert.merged) {
+                                // start at index 1 to avoid re-invoking component mounted hook
+                                for (var i$2 = 1; i$2 < insert.fns.length; i$2++) {
+                                    insert.fns[i$2]();
+                                }
+                            }
+                        } else {
+                            registerRef(ancestor);
+                        }
+                        ancestor = ancestor.parent;
                     }
                 }
-                oldVnode = emptyNodeAt(oldVnode);
+    
+                // destroy old node
+                if (isDef(parentElm)) {
+                    removeVnodes([oldVnode], 0, 0);
+                } else if (isDef(oldVnode.tag)) {
+                    invokeDestroyHook(oldVnode);
+                }
             }
         }
     }
